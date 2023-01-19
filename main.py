cb946a37d11b473437ca21f9ee1450019237cc8f
rumon5h <mdrumonkhan48519@gmail.com>
2023-01-19 23:19:32 +0600

    created the body

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print('calcularor')
+from tkinter import *
+
+def button_press(num):
+    pass
+
+def equals():
+    pass
+
+def clear():
+    pass
+
+
+window = Tk()
+window.title("Calculator program")
+window.geometry("500x500")
+
+equation_text = ""
+equation_label = StringVar()
+
+lable = Label(window, textvariable=equation_label, font=('consolas', 20), bg='white', width=24, height=2)
+lable.pack()
+
+window.mainloop()
