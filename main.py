a66ffdfe9635dca1e10212620927193d85faf847
rumon5h <mdrumonkhan48519@gmail.com>
2023-01-20 09:39:16 +0600

    button 7 added|

--- main.py
+++ main.py
@@ -47,4 +47,11 @@ button5.grid(row=1, column=1)
 button6 = Button(frame, text=6, height=4, width=9, font=35, command= lambda: button_press(6))
 button6.grid(row=1, column=2)
 
+
+
+button7 = Button(frame, text=7, height=4, width=9, font=35, command= lambda: button_press(7))
+button7.grid(row=2, column=0)
+
+
+
 window.mainloop()
