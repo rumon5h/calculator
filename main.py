46235cb4feb8c36fd094b4e0b1b5176aecfef275
rumon5h <mdrumonkhan48519@gmail.com>
2023-01-20 09:54:30 +0600

    divide button added

--- main.py
+++ main.py
@@ -75,4 +75,8 @@ multiply = Button(frame, text='*', height=4, width=9, font=35, command= lambda: 
 multiply.grid(row=2, column=3)
 
 
+divide = Button(frame, text='/', height=4, width=9, font=35, command= lambda: button_press('/'))
+divide.grid(row=3, column=3)
+
+
 window.mainloop()
